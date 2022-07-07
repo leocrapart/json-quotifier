25f57371b61f7d96f054b61f928f03c04212c4e5
leo crapart <leocrapart@yahoo.fr>
2022-07-07 12:25:46 +0200

    double quote gherkin files

--- src/json_quotifier/core.clj
+++ src/json_quotifier/core.clj
@@ -10,38 +10,42 @@
 (defn single-quote-string [double-quote-string]
   (str/escape double-quote-string {\" "'"}))
 
+; api
 (defn double-quote-string [single-quote-string]
   (str/escape single-quote-string {\' "\""}))
 
-(single-quote-string "\"field1\"")
-(single-quote-string "\"field1\":\"value1\"")
-(single-quote-string "{\"field1\":\"value1\"}")
+(comment
+  (single-quote-string "\"field1\"")
+  (single-quote-string "\"field1\":\"value1\"")
+  (single-quote-string "{\"field1\":\"value1\"}")
 
-(single-quote-string "\"field1\"")
-(double-quote-string "'field1'")
-
-
-;; => "'field1'"
-(println 
-  (str "'lala' \"lala\""))
+  (single-quote-string "\"field1\"")
+  (double-quote-string "'field1'")
 
 
 
-;; tests
-;; simple double-quoter works fine
-;; single-quoter impossible (needs \", can't interpret raw double quotes)
 
-(def test-json
-  "{
-		'$id': 'id_1_test_qa',
-		'$pk': 'pk_1_test_qa',
-		'$model': 'test_model',
-		'field1': [[  ['value1[0][0][0]', 'value1[0][0][1]']  , 'value1[0][1]'], 'value1[1]']
-		}")
+  ;; => "'field1'"
+  (println 
+    (str "'lala' \"lala\""))
 
 
-(println
-  (double-quote-string test-json))
+
+  ;; tests
+  ;; simple double-quoter works fine
+  ;; single-quoter impossible (needs \", can't interpret raw double quotes)
+
+  (def test-json
+    "{
+      '$id': 'id_1_test_qa',
+      '$pk': 'pk_1_test_qa',
+      '$model': 'test_model',
+      'field1': [[  ['value1[0][0][0]', 'value1[0][0][1]']  , 'value1[0][1]'], 'value1[1]']
+      }")
+
+
+  (println
+    (double-quote-string test-json)))
 
 
 
@@ -56,3 +60,57 @@
 ;; -clean color highlight (like sublime default colors for clojure/script)
 ;; -paredit parinfer stuff working fine
 ;; -all in one (cmd into the IDE, or easy shortcut to open with good path)
+
+(comment 
+  "double quote the gherkin files of the Digital twin specflow project"
+  "creates v2 files at their side with the new double quoted format"
+
+  (def gherkin-file-path "C:\\Users\\l.crapart\\source\\repos\\DigitalGraphData\\tests\\DigitalTwin.tests.IntegrationTests\\Features\\Update\\UpdateEntityPatch_BasicPropertiesWithPartialJson.feature")
+
+  (def gherkin-file (slurp gherkin-file-path))
+
+  (type gherkin-file)
+  (println gherkin-file)
+
+
+  (def double-quote-gherkin-file (double-quote-string gherkin-file))
+
+  (println double-quote-gherkin-file)
+
+  (def output-gherkin-file-path "C:\\Users\\l.crapart\\source\\repos\\DigitalGraphData\\tests\\DigitalTwin.tests.IntegrationTests\\Features\\Update\\UpdateEntityPatch_BasicPropertiesWithPartialJson2.feature")
+  (spit output-gherkin-file-path double-quote-gherkin-file)
+
+
+  (def add-feature-path "C:\\Users\\l.crapart\\source\\repos\\DigitalGraphData\\tests\\DigitalTwin.tests.IntegrationTests\\Features\\Update\\UpdateEntityPatch_AddItemArray.feature")
+  (def remove-feature-path "C:\\Users\\l.crapart\\source\\repos\\DigitalGraphData\\tests\\DigitalTwin.tests.IntegrationTests\\Features\\Update\\UpdateEntityPatch_RemoveItemArray.feature")
+  (def update-feature-path "C:\\Users\\l.crapart\\source\\repos\\DigitalGraphData\\tests\\DigitalTwin.tests.IntegrationTests\\Features\\Update\\UpdateEntityPatch_UpdateArrayItem.feature")
+  (def path-feature-path "C:\\Users\\l.crapart\\source\\repos\\DigitalGraphData\\tests\\DigitalTwin.tests.IntegrationTests\\Features\\Update\\UpdateEntityPatch_BasicPropertiesWithPath.feature")
+
+
+  (defn path-without-end-feature [path]
+    ((clojure.string/split path #".feature") 0))
+
+  (path-without-end-feature add-feature-path)
+
+  (def add-feature-path-2 (str (path-without-end-feature add-feature-path) "2.feature"))
+  (def remove-feature-path-2 (str (path-without-end-feature remove-feature-path) "2.feature"))
+  (def update-feature-path-2 (str (path-without-end-feature update-feature-path) "2.feature"))
+  (def path-feature-path-2 (str (path-without-end-feature path-feature-path) "2.feature"))
+
+  add-feature-path-2
+  remove-feature-path-2
+  update-feature-path-2
+  path-feature-path-2
+
+
+  (spit add-feature-path-2 
+    (double-quote-string (slurp add-feature-path)))
+
+  (spit remove-feature-path-2 
+    (double-quote-string (slurp remove-feature-path)))
+
+  (spit update-feature-path-2 
+    (double-quote-string (slurp update-feature-path)))
+
+  (spit path-feature-path-2 
+    (double-quote-string (slurp path-feature-path))))
